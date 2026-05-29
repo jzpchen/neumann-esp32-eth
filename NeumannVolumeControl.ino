85bfc9173023d8db6378977659651a67609fa993
jzpchen <jzpchen@yahoo.com>
2026-05-29 15:40:37 -0700

    Reassign I2C SCL pin to GPIO 16 (Pin 32) to resolve RGB LED pin conflict

--- NeumannVolumeControl.ino
+++ NeumannVolumeControl.ino
@@ -259,7 +259,7 @@ void setup() {
   Serial.println("REST API WebServer started on port 5000");
 
   // Initialize I2C and OLED
-  Wire.begin(17, 21); // Pin 34 (SDA = 17), Pin 35 (SCL = 21)
+  Wire.begin(17, 16); // Pin 34 (SDA = 17), Pin 32 (SCL = 16)
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
   } else {
